0f8d626cb85e47e10653435650719cc7655c9565
ryanfakir <ryanfakir@gmail.com>
2017-01-10 21:15:35 -0500

    add new version

--- go/src/leetcode_139.go
+++ go/src/leetcode_139.go
@@ -1,22 +1,21 @@
 package main
 
 func wordBreak(s string, wordDict []string) bool {
-	l := len(s)
-	if l == 0 {
-		return true
-	}
-	for i := 0; i < l; i++ {
-		if contains(wordDict, s[:i+1]) {
-			if wordBreak(s[i+1:], wordDict) {
-				return true
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i < len(s)+1; i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && contains(s[j:i], wordDict) {
+				dp[i] = true
+				break
 			}
 		}
 	}
-	return false
+	return dp[len(s)]
 }
 
-func contains(wordDict []string, target string) bool {
-	for _, v := range wordDict {
+func contains(target string, dict []string) bool {
+	for _, v := range dict {
 		if v == target {
 			return true
 		}
