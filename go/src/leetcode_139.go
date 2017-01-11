d6851a07863fecfc9156a6339a5c0bb41eb1ed67
ryanfakir <ryanfakir@gmail.com>
2017-01-10 21:17:52 -0500

    add blog

--- go/src/leetcode_139.go
+++ go/src/leetcode_139.go
@@ -22,3 +22,27 @@ func contains(target string, dict []string) bool {
 	}
 	return false
 }
+
+func wordBreak(s string, wordDict []string) bool {
+	l := len(s)
+	if l == 0 {
+		return true
+	}
+	for i := 0; i < l; i++ {
+		if contains(wordDict, s[:i+1]) {
+			if wordBreak(s[i+1:], wordDict) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func contains(wordDict []string, target string) bool {
+	for _, v := range wordDict {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
