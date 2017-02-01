e9fe225eee2bc0ec7625ecf4579af54a73933351
ryanfakir <ryanfakir@gmail.com>
2017-01-31 23:15:09 -0500

    go code

---
+++ go/src/leetcode_91.go
@@ -0,0 +1,34 @@
+package main
+
+import "strconv"
+
+func numDecodings(s string) int {
+	if len(s) == 0 || s[0] == '0' {
+		return 0
+	}
+	dp := make([]int, len(s))
+	dp[0] = 1
+	for i := 1; i < len(s); i++ {
+		for j := i - 1; j <= i; j++ {
+			if valid(s[j : i+1]) {
+				if j-1 < 0 {
+					dp[i] += 1
+				} else {
+					dp[i] += dp[j-1]
+				}
+			}
+		}
+	}
+	return dp[len(s)-1]
+}
+
+func valid(s string) bool {
+	if s[0] == '0' {
+		return false
+	}
+	val, _ := strconv.Atoi(s)
+	if val > 26 {
+		return false
+	}
+	return true
+}
