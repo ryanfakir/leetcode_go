a8f53ca021c9b626d08b1fee33de6287086fee15
ryanfakir <ryanfakir@gmail.com>
2017-02-11 21:59:37 -0500

    go code

---
+++ go/src/leetcode_42.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+func trap(height []int) int {
+	if len(height) == 0 {
+		return 0
+	}
+	left, right := 0, len(height)-1
+	var res int
+	for left < right {
+		min := int(math.Min(float64(height[left]), float64(height[right])))
+		if min == height[left] {
+			left++
+			for left < right && height[left] < min {
+				res += min - height[left]
+				left++
+			}
+		} else {
+			right--
+			for left < right && height[right] < min {
+				res += min - height[right]
+				right--
+			}
+		}
+	}
+	return res
+}
