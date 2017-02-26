0a96fd56abcc74b3b9dc234da3e862b505b94390
ryanfakir <ryanfakir@gmail.com>
2017-02-25 21:48:06 -0500

    go code

--- go/src/leetcode_49.go
+++ go/src/leetcode_49.go
@@ -20,3 +20,19 @@ func groupAnagrams(strs []string) [][]string {
 	}
 	return res
 }
+func groupAnagrams(strs []string) [][]string {
+	var res [][]string
+	sort.Strings(strs)
+	m := make(map[string][]string)
+	for _, v := range strs {
+		org := v
+		temp := strings.Split(v, "")
+		sort.Strings(temp)
+		v = strings.Join(temp, "")
+		m[v] = append(m[v], org)
+	}
+	for _, v := range m {
+		res = append(res, v)
+	}
+	return res
+}
