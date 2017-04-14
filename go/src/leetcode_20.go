004c3a8fe88c227f325311317cc34f77a7c64381
ryanfakir <ryanfakir@gmail.com>
2017-04-13 22:23:56 -0400

    go code

--- go/src/leetcode_20.go
+++ go/src/leetcode_20.go
@@ -1,19 +1,36 @@
 package src
 
 func isValid(s string) bool {
-    dict := make(map[rune]rune, 0)
-    dict['}'] = '{'
-    dict[')'] = '('
-    dict[']'] = '['
-    stack := make([]rune, 0)
-    for _, e := range s {
-        if len(stack) == 0 {
-            stack = append(stack, e)
-        } else if dict[e] == stack[len(stack)-1] {
-            stack = stack[:len(stack)-1]
-        } else {
-            stack = append(stack, e)
-        }
-    }
-    return len(stack) == 0
+	dict := make(map[rune]rune, 0)
+	dict['}'] = '{'
+	dict[')'] = '('
+	dict[']'] = '['
+	stack := make([]rune, 0)
+	for _, e := range s {
+		if len(stack) == 0 {
+			stack = append(stack, e)
+		} else if dict[e] == stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, e)
+		}
+	}
+	return len(stack) == 0
+}
+func isValid(s string) bool {
+	dict := make(map[rune]rune, 0)
+	dict['}'] = '{'
+	dict[')'] = '('
+	dict[']'] = '['
+	var stack []rune
+	for _, e := range s {
+		if len(stack) == 0 {
+			stack = append(stack, e)
+		} else if dict[e] == stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, e)
+		}
+	}
+	return len(stack) == 0
 }
