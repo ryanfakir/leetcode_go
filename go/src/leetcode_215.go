7950c1035149e36f9a0a3c8f19d7954957eb5d06
ryanfakir <ryanfakir@gmail.com>
2017-04-17 23:25:40 -0400

    go code

--- go/src/leetcode_215.go
+++ go/src/leetcode_215.go
@@ -1,5 +1,7 @@
 package main
 
+import "container/heap"
+
 func findKthLargest(nums []int, k int) int {
 	k = len(nums) - k
 	start, end := 0, len(nums)-1
@@ -39,4 +41,35 @@ func helper(nums []int, start, end int) int {
 	nums[pvIndex] = nums[start]
 	nums[start] = temp
 	return start
+}
+func findKthLargest(nums []int, k int) int {
+	// h := make(IntElement, len(nums))
+	var h IntElement
+	heap.Init(&h)
+	for _, v := range nums {
+		// h[i] = v
+		heap.Push(&h, v)
+	}
+	// heap.Init(&h)
+	for len(nums)-k > 0 {
+		k++
+	}
+	return heap.Pop(&h).(int)
+}
+
+type IntElement []int
+
+func (el IntElement) Swap(i, j int)      { el[i], el[j] = el[j], el[i] }
+func (el IntElement) Less(i, j int) bool { return el[i] < el[j] }
+func (el IntElement) Len() int           { return len(el) }
+
+func (p *IntElement) Push(x interface{}) {
+	*p = append(*p, x.(int))
+}
+
+func (p *IntElement) Pop() interface{} {
+	temp := *p
+	el := temp[len(temp)-1]
+	*p = temp[:len(temp)-1]
+	return el
 }
