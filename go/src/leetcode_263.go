bc5629003c75e6b0ad303ba7a7b7df982272911f
ryanfakir <ryanfakir@gmail.com>
2017-04-20 22:48:55 -0400

    go code

--- go/src/leetcode_263.go
+++ go/src/leetcode_263.go
@@ -1,24 +1,25 @@
 package src
 
 func isUgly(num int) (res bool) {
-    if num < 0 {
-        return
-    }
-    temp := num
-    for temp > 0 {
-        if temp % 2 == 0 {
-            temp /= 2
-        } else if temp % 3 == 0 {
-            temp /= 3
-        } else if temp % 5 == 0 {
-            temp /= 5
-        } else if temp == 1 {
-            res = true
-            break
-        } else {
-            res = false
-            break
-        }
-    }
-    return
+	if num < 0 {
+		return
+	}
+	temp := num
+
+	for temp > 0 {
+		if temp%2 == 0 {
+			temp /= 2
+		} else if temp%3 == 0 {
+			temp /= 3
+		} else if temp%5 == 0 {
+			temp /= 5
+		} else if temp == 1 {
+			res = true
+			break
+		} else {
+			res = false
+			break
+		}
+	}
+	return
 }
