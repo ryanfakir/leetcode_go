73f114ec3577c97245c292f95301118ad1dc4c59
ryanfakir <ryanfakir@gmail.com>
2017-05-27 16:50:01 -0400

    go code

--- go/src/leetcode_144.go
+++ go/src/leetcode_144.go
@@ -1,18 +1,29 @@
 package src
 
 func preorderTraversal(root *TreeNode) []int {
-    temp := root
-    stack := make([]*TreeNode, 0)
-    res := make([]int, 0)
-    for temp != nil || len(stack) != 0  {
-        for temp != nil {
-            stack = append(stack, temp)
-            res = append(res, temp.Val)
-            temp = temp.Left
-        }
-        pop:= stack[len(stack) -1]
-        stack = stack[:len(stack)-1]
-        temp = pop.Right
-    }
-    return res
+	temp := root
+	stack := make([]*TreeNode, 0)
+	res := make([]int, 0)
+	for temp != nil || len(stack) != 0 {
+		for temp != nil {
+			stack = append(stack, temp)
+			res = append(res, temp.Val)
+			temp = temp.Left
+		}
+		pop := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		temp = pop.Right
+	}
+	return res
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	var res []int
+	res = append(res, root.Val)
+	res = append(res, preorderTraversal(root.Left)...)
+	res = append(res, preorderTraversal(root.Right)...)
+	return res
 }
