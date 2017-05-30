145ae7ad10b70763af62c9aa438eb80595cb592a
ryanfakir <ryanfakir@gmail.com>
2017-05-29 23:42:41 -0400

    go code

---
+++ go/src/leetcode_450.go
@@ -0,0 +1,28 @@
+package main
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val > key {
+		root.Left = deleteNode(root.Left, key)
+	} else if root.Val < key {
+		root.Right = deleteNode(root.Right, key)
+	} else {
+		if root.Left == nil || root.Right == nil {
+			if root.Left != nil {
+				root = root.Left
+			} else {
+				root = root.Right
+			}
+		} else {
+			temp := root.Right
+			for temp.Left != nil {
+				temp = temp.Left
+			}
+			root.Val = temp.Val
+			root.Right = deleteNode(root.Right, root.Val)
+		}
+	}
+	return root
+}
