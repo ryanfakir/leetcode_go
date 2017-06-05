13c8ad8f9fa917a2b47b45d036c79672f6f61951
ryanfakir <ryanfakir@gmail.com>
2017-06-04 22:56:05 -0400

    go code

--- go/src/leetcode_35.go
+++ go/src/leetcode_35.go
@@ -27,3 +27,15 @@ func searchInsert(nums []int, target int) int {
 	}
 
 }
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] >= target {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
