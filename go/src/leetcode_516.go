4638ac4172ea3b48ee11662f3c87ffb084fcc9bf
ryanfakir <ryanfakir@gmail.com>
2017-06-16 22:23:09 -0400

    go code

---
+++ go/src/leetcode_516.go
@@ -0,0 +1,21 @@
+package main
+
+import "math"
+
+func longestPalindromeSubseq(s string) int {
+	dp := make([][]int, len(s))
+	for i, _ := range dp {
+		dp[i] = make([]int, len(s))
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		dp[i][i] = 1
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = int(math.Max(float64(dp[i+1][j]), float64(dp[i][j-1])))
+			}
+		}
+	}
+	return dp[0][len(s)-1]
+}
