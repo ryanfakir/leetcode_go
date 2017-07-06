fae3677ae887314adcad009639858ef177ca61bd
ryanfakir <ryanfakir@gmail.com>
2017-07-05 22:47:44 -0400

    go code

--- go/src/leetcode_207.go
+++ go/src/leetcode_207.go
@@ -30,3 +30,33 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	}
 	return true
 }
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	graph := make(map[int][]int)
+	for _, v := range prerequisites {
+		graph[v[0]] = append(graph[v[0]], v[1])
+	}
+	visited := make(map[int]bool)
+	stack := make(map[int]bool)
+	for i := 0; i < numCourses; i++ {
+		if dfs(i, visited, stack, graph) {
+			return false
+		}
+	}
+	return true
+}
+
+func dfs(node int, visited, stack map[int]bool, graph map[int][]int) bool {
+	if !visited[node] {
+		visited[node] = true
+		stack[node] = true
+		for _, v := range graph[node] {
+			if !visited[v] && dfs(v, visited, stack, graph) {
+				return true
+			} else if stack[v] {
+				return true
+			}
+		}
+	}
+	stack[node] = false
+	return false
+}
