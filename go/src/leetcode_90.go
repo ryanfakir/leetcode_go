c38e54c372dc676604b564d10b1115b3a00537ca
ryanfakir <ryanfakir@gmail.com>
2017-07-06 23:19:14 -0400

    go code

--- go/src/leetcode_90.go
+++ go/src/leetcode_90.go
@@ -26,3 +26,23 @@ func helper(nums []int, temp []int, res [][]int, visited int, level int) [][]int
 	}
 	return res
 }
+func subsetsWithDup(nums []int) [][]int {
+	sort.Ints(nums)
+	return dfs(nil, nil, nums, 0)
+}
+
+func dfs(res [][]int, temp, nums []int, level int) [][]int {
+	real := make([]int, len(temp))
+	copy(real, temp)
+	res = append(res, real)
+	levelMap := make(map[int]bool)
+	for i := level; i < len(nums); i++ {
+		if !levelMap[nums[i]] {
+			levelMap[nums[i]] = true
+			temp = append(temp, nums[i])
+			res = dfs(res, temp, nums, i+1)
+			temp = temp[:len(temp)-1]
+		}
+	}
+	return res
+}
