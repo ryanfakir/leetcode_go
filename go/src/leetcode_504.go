01224d3c7d1188380d8e8f58397dde1eecfe378e
ryanfakir <ryanfakir@gmail.com>
2017-07-29 23:46:35 -0400

    go code

---
+++ go/src/leetcode_504.go
@@ -0,0 +1,14 @@
+package main
+
+import "strconv"
+
+func convertToBase7(num int) string {
+	if num < 0 {
+		return "-" + convertToBase7(-num)
+	}
+	if num < 7 {
+		return strconv.Itoa(num)
+	}
+	return convertToBase7(num/7) + strconv.Itoa(num%7)
+
+}
