9befe6b2af5c45401928be6d9eb84a13b789c878
ryanfakir <ryanfakir@gmail.com>
2018-06-14 02:32:37 -0400

    614

--- go/src/leetcode_5.go
+++ go/src/leetcode_5.go
@@ -29,3 +29,34 @@ func helper(left int, right int, s string) (res string) {
 	}
 	return
 }
+
+func longestPalindrome(s string) string {
+    var res string
+    for i := range s {
+        single := isPalindrome(s, i, i)
+        double := isPalindrome(s, i, i+1)
+        if len(single) > len(double) {
+            if len(single) > len(res) {
+                res = single
+            }
+        } else {
+            if len(double) > len(res) {
+                res = double
+            }
+        }
+    }
+    return res
+}
+
+func isPalindrome(s string, left, right int) string {
+    var res string
+    for left >= 0 && right < len(s) {
+        if s[left] != s[right] {
+            break
+        }
+        res = s[left: right+1]
+        left--
+        right++
+    }
+    return res
+}
