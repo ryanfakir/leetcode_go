e629d0e5a52d66d90c43fec91580170b10ac8a4d
ryanfakir <ryanfakir@gmail.com>
2018-06-24 23:29:56 -0700

    624

--- go/src/leetcode_74.go
+++ go/src/leetcode_74.go
@@ -23,3 +23,21 @@ func searchMatrix(matrix [][]int, target int) bool {
 		return false
 	}
 }
+
+func searchMatrix(matrix [][]int, target int) bool {
+    if len(matrix) == 0 || len(matrix[0]) == 0 {return false}
+    m, n := len(matrix), len(matrix[0])
+    left ,right := 0, m * n - 1
+    for left <= right {
+        
+        mid := (left + right)/2
+        if matrix[mid/n][mid%n] == target {
+            return true
+        } else if matrix[mid/n][mid%n] > target {
+            right = mid -1
+        } else {
+            left = mid +1
+        }
+    }
+    return false
+}
