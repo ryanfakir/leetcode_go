033b774dc1ebe39b028ca11fea541c867a962c8e
ryanfakir <ryanfakir@gmail.com>
2018-06-28 23:26:28 -0700

    628

--- go/src/leetcode_92.go
+++ go/src/leetcode_92.go
@@ -21,3 +21,20 @@ func reverseBetween(head *ListNode, m int, n int) *ListNode {
 	}
 	return start.Next
 }
+
+func reverseBetween(head *ListNode, m int, n int) *ListNode {
+    dummy := &ListNode{-1, head}
+    prev := dummy
+    for i := 1 ; i < m; i++ {
+        prev = prev.Next
+    }
+    first, sec := prev.Next, prev.Next.Next
+    for sec != nil && m < n {
+        first.Next = sec.Next
+        sec.Next = prev.Next
+        prev.Next = sec
+        sec = first.Next
+        m++
+    }
+    return dummy.Next
+}
