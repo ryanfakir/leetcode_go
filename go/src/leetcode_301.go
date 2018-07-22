e55aa3ace43356fe560b63ac23c8f982abccc9a9
ryanfakir <ryanfakir@gmail.com>
2018-07-21 23:19:28 -0700

    0721

--- go/src/leetcode_301.go
+++ go/src/leetcode_301.go
@@ -49,3 +49,48 @@ func valid(s string) bool {
 	}
 	return cnt == 0
 }
+
+var res []string
+func removeInvalidParentheses(s string) []string {
+    res = nil
+    var q = []string{s}
+    for len(q) > 0 {
+        size := len(q)
+        var bingo bool
+        visited := make(map[string]bool)
+        for i:= 0; i < size; i++ {
+            pop := q[0]
+            q = q[1:]
+            if valid(pop) {
+                bingo = true
+                res = append(res, pop)
+                continue
+            }
+            for j:=0; j< len(pop); j++ {
+                if pop[j] != '(' && pop[j] != ')' {continue}
+                tmp := pop[:j] + pop[j+1:]
+                if !visited[tmp] {
+                    visited[tmp] = true
+                    q =append(q, tmp)
+                }
+            }
+        }
+        if bingo {
+            break
+        }
+    }
+    return res
+}
+
+func valid(s string) bool {
+    var left, right int
+    for i:=0; i < len(s); i++ {
+        if s[i] == '(' {left++}
+        if s[i] == ')' {right++}
+        if left < right {
+            return false
+        }
+    }
+    return left == right
+}
+
