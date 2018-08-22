32b69b89ae480d0acca944cc17b8f2838b37fdd4
ryanfakir <ryanfakir@gmail.com>
2018-08-21 23:17:11 -0700

    0821

--- go/src/leetcode_786.go
+++ go/src/leetcode_786.go
@@ -1,18 +1,16 @@
 func kthSmallestPrimeFraction(A []int, K int) []int {
     left, right := 0.0, 1.0
-    p, q := 0, 1
     for {
         mid := (left + right)/2
-        fmt.Println(mid)
         var cnt int
+        p, q := 0, 1
         for i, j := 0, 0; i < len(A); i++ {
             for j < len(A) && float64(A[i]) > mid * float64(A[j]) {j++}
             cnt += len(A) - j
-            if j < len(A) && p* A[i] <  A[j] * q {
+            if j < len(A) && p* A[j] <  A[i] * q {
                 p, q = A[i], A[j]
             }
         }
-        fmt.Println(cnt)
         if cnt == K {
             return []int{p, q}
         } else if cnt < K {
