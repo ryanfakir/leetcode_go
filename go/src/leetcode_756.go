370e3b9407f230f8148dcb2b61f34060451f6ed3
ryanfakir <ryanfakir@gmail.com>
2018-08-30 20:46:55 -0700

    0830

--- go/src/leetcode_756.go
+++ go/src/leetcode_756.go
@@ -17,3 +17,20 @@ func dfs(b, tmp string, start int,  dict map[string][]string) bool {
     }
     return false
 }
+
+func pyramidTransition(bottom string, allowed []string) bool {
+    dict := make(map[string][]string)
+    for _ , a :=range allowed {
+        dict[a[:2]] = append(dict[a[:2]], a[2:])
+    }
+    return dfs(bottom, "", dict)
+}
+
+func dfs(low, up string, dict map[string][]string) bool {
+    if len(up) == 1 && len(low) == 2{return true}
+    if len(up) == len(low) -1 {return dfs(up, "", dict)}
+    for _, v := range dict[low[len(up): len(up)+2]] {
+        if dfs(low, up + v, dict) {return true}  
+    }
+    return false
+}
